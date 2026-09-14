38a799d3ccf845280c62d883ea45a4294bea8456
PintoJR <guimoraespinto@gmail.com>
2026-09-14 15:02:01 -0300

    Solution03

--- app/main.py
+++ app/main.py
@@ -1,45 +1,54 @@
 class SoftwareEngineer:
     def __init__(self, name: str, skills: list = None) -> None:
-        self.name = name,
-        self.skills = skills,
+        self.name = name
+        self.skills = skills if skills is not None else []
+
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
 
 
 class FrontendDeveloper(SoftwareEngineer):
-    def __init__(self, name: str, skills: list = ["JavaScript", "HTML", "CSS"]) -> None:
-        super(SoftwareEngineer, self).__init__(name, skills)
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.skills.extend(["JavaScript", "HTML", "CSS"])
 
-    def create_awesome_web_page(self):
+
+    def create_awesome_web_page(self) -> str:
         print(f"{self.name} is creating a webpage...")
         return "<h1>Hello world</h1>"
 
 
 class BackendDeveloper(SoftwareEngineer):
-    def __init__(self, name, skills = ["Python", "SQL", "Django"]):
-        super(SoftwareEngineer).__init__(name, skills)
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.skills.extend(["Python", "SQL", "Django"])
 
-    def create_powerful_api(self):
+    def create_powerful_api(self) -> str:
         print(f"{self.name} is creating an API...")
         return "http://127.0.0.1:8000"
 
 
 class AndroidDeveloper(SoftwareEngineer):
-    def __init__(self, name, skills = ["Java", "Android studio"]):
-        super(SoftwareEngineer).__init__(name, skills)
+    class BackendDeveloper(SoftwareEngineer):
+        def __init__(self, name: str):
+            super().__init__(name)
+            self.skills.extend(["Java", "Android studio"])
 
-    def create_smooth_mobile_app(self):
+
+    def create_smooth_mobile_app(self) -> str:
         print(f"{self.name} is creating a mobile app...")
         return "Ads every three swipes"
 
 
 class FullStackDevelope(SoftwareEngineer, AndroidDeveloper,
                         BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name, skills = ["Java", "Android studio"]):
-        super(SoftwareEngineer).__init__(name, skills)
+    def __init__(self, name: str):
+            super().__init__(name)
+            self.skills.extend()
 
-    def create_web_application(self):
+    def create_web_application(self) -> str:
         print(f"{self.name} started creating a web application...")
-        self.create_powerful_api
-        self.create_awesome_web_page
+        self.create_powerful_api()
+        self.create_awesome_web_page()
+
