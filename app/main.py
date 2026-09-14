79c9321267832180f1891304a3c40e06850cd642
PintoJR <guimoraespinto@gmail.com>
2026-09-14 16:15:43 -0300

    Solution08

--- app/main.py
+++ app/main.py
@@ -37,10 +37,9 @@ class AndroidDeveloper(SoftwareEngineer):
         return "Ads every three swipes"
 
 
-class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.skills.extend(["Python", "SQL", "Django"])
+        super().__init__(name) 
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
